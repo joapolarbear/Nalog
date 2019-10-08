e2eaba030a11946e3bdd2c5caf1eedab386fcad1
hphu <huhanpeng@bytedance.com>
2019-10-08 20:29:57 +0800

    Initial Commit

---
+++ Nalog/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nalog
+//
+//  Created by huhanpeng on 2019/10/8.
+//  Copyright © 2019 huhanpeng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Nalog/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nalog
+//
+//  Created by huhanpeng on 2019/10/8.
+//  Copyright © 2019 huhanpeng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
